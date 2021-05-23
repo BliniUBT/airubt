f3e0f2df790ab3ad2e5b9516eee200d80b970c02
Çlirim Krasniqi <qliraa19@gmail.com>
2021-05-23 23:37:20 +0200

    Banner Done

--- my-app/my-app/src/components/Banner.js
+++ my-app/my-app/src/components/Banner.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import '../assets/css/Banner.css';
+import '../assets/css/banner.css';
 
 function Banner(){
     return(
-        <div class='banner'>
-            {/*Header inside banner */}
-            <h1>I am banner</h1>
+        <div className='banner'>
+            {/*Header inside banner1 */}
+            <div className="banner-info">
+                <h1>Your go-to travel guide</h1>
+                <h2>find the best place for <span id='you'>you</span></h2>
+                <button id='banner-btn1'>Find now</button>
+                <button id='banner-btn2'>Learn more</button>
+                {/* <input type="submit" id="banner-btn1"/> */}
+                
+            </div>
         </div>
 
 
