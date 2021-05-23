a339ca51ed0e0542544faf41af254083a8a8bd8b
BliniUBT <bk48088@ubt-uni.net>
2021-05-24 01:02:55 +0200

    Header permbi Banner duhet

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -4,13 +4,13 @@ import "./assets/css/style.css";
 import Home from "./components/Home";
 import Header from "./components/Header";
 
-
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
       {/* Banner */}
-      <Header />
+
       {/* Banner */}
       {/* Cards */}
       {/* Footer */}
