bd6c5be535026183e15d190aab992744afba123b
Çlirim Krasniqi <qliraa19@gmail.com>
2021-05-25 03:43:26 +0200

    Finished Slide show

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -1,18 +1,19 @@
-
-import './App.css';
 import './index.css';
 import './assets/css/style.css';
 // import Home from './components/Home';
 import Footer from './components/Footer';
-import Banner from './components/Banner';
+// import Banner from './components/Banner';
 import Cards from './components/Cards';
+import ImageSlider from './components/ImageSlider';
+import { SliderData } from './components/SliderData';
+
 
 
 function App() {
   return (
     <div className="App">
       {/* Banner */}
-      <Banner />
+      <ImageSlider slides={SliderData} />
       {/* Banner */}
       {/* Cards */}
 
