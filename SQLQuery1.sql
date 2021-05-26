e57099135a6c1a08c096e488044d72a45487bed4
HanaMacula1 <hm47547@ubt-uni.net>
2021-05-26 15:46:03 +0200

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,16 @@
 Create Database airubt
 use airubt
+
+create table Users(
+	UserID int identity(1,1) primary key,
+	Name varchar(250) not null,
+	LastName varchar(250) not null,
+	DateOfBirth date not null,
+	Age DATEDIFF(YEAR, DateOfBirth, GETDATE()),
+	Address varchar(250) not null,
+	Email varchar(250) not null,
+	Password varchar(250) not null,
+	Role bit
+)
+
+
