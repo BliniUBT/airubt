adc7b793c115aee113323e3b37ef6c8b384a408a
HanaMacula1 <hm47547@ubt-uni.net>
2021-05-26 15:54:10 +0200

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -15,12 +15,12 @@ Alter table Users add Age as DATEDIFF(YEAR, DateOfBirth, GETDATE())
 
 
 Create table City(
-CityName varchar primary key,
-ZipCode int  not null,
-Shteti varchar(100) not null
+	CityName varchar primary key,
+	ZipCode int  not null,
+	Shteti varchar(100) not null
 )
 
 Create table Categories(
-CategoriesName varchar(50) primary key
+	CategoriesName varchar(50) primary key
 )
 
