1e29ad1be3c286b87b4781bb169540316c7d087c
HanaMacula1 <hm47547@ubt-uni.net>
2021-05-26 16:28:30 +0200

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,15 +6,26 @@ create table Users(
 	Name varchar(250) not null,
 	LastName varchar(250) not null,
 	DateOfBirth date not null,
-	Address varchar(250) not null,
 	Email varchar(250) not null,
 	Password varchar(250) not null,
-	Role bit
 )
-Alter table Users add Age as DATEDIFF(YEAR, DateOfBirth, GETDATE())\
+Alter table Users add Age as DATEDIFF(YEAR, DateOfBirth, GETDATE())
 ALter table Users add PhoneNumber varchar(100)
-Alter table Users drop column Address
+
 Select * from Users
+---------------------------------------------------------------------------------------------------------------------
+create table Hosts(
+	HostID int identity(1,1) primary key,
+	Name varchar(250) not null,
+	LastName varchar(250) not null,
+	DateOfBirth date not null,
+	Email varchar(250) not null,
+	Password varchar(250) not null,
+)
+Alter table Users add Age as DATEDIFF(YEAR, DateOfBirth, GETDATE())
+ALter table Users add PhoneNumber varchar(100)
+
+---------------------------------------------------------------------------------------------------------------------
 
 Create table City(
 	CityName varchar primary key,
@@ -22,9 +33,13 @@ Create table City(
 	Shteti varchar(100) not null
 )
 
+---------------------------------------------------------------------------------------------------------------------
+
+
 Create table Categories(
 	CategoriesName varchar(50) primary key
 )
 
+---------------------------------------------------------------------------------------------------------------------
 
 
