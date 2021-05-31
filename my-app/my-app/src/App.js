f15dcc604fe26810f8cbdb333787b0429fda7798
BliniUBT <bk48088@ubt-uni.net>
2021-05-31 02:37:46 +0200

    Included changes on Router where it is based on App.js. Footer had to be included and insert it as a route and apper globally.
    Dea <3oxha

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -1,14 +1,18 @@
 import './index.css';
 import './assets/css/style.css';
 import ForLogin from './components/login/forlogin';
-import Header from './components/Header'
 
-// import Home from './components/Home';
+// PARTIALS
+import Header from './components/Header';
 import Footer from './components/Footer';
+// PARTIALS END
+
 // import Banner from './components/Banner';
 import Cards from './components/Cards';
 import ImageSlider from './components/ImageSlider';
 import {SliderData} from './components/SliderData';
+
+
 import {Router, Link, BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
 import Admin from './admin/components/Admin';
@@ -18,6 +22,7 @@ function App() {
         <div className="App">
             <BrowserRouter>
                 <Route path="/" component={Header}/>
+                <Route path="/" component={Footer}/>
                 <Route exact path="/" component={Home}/>
                 <Route exact path="/login" component={ForLogin}/>
                 <Route path="/admin" component={Admin}/>
