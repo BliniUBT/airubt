f023b32efa72cc3fc55c135befc886297876c3cb
BliniUBT <bk48088@ubt-uni.net>
2021-05-31 02:55:24 +0200

    Included React-Dom-Router to Header which we had to link register/login Modal.

--- my-app/my-app/src/components/Header.js
+++ my-app/my-app/src/components/Header.js
@@ -2,26 +2,32 @@ import React from "react";
 import "../assets/css/header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import FlightIcon from "@material-ui/icons/Flight";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Admin from "../admin/components/Admin"
+import './login/index'
+import {BrowserRouter, Route,Link, Switch} from 'react-router-dom';
+
+// import Admin from "../admin/components/Admin"
 
 function Header() {
-  return (
-    <div className="header">
-      <FlightIcon className="header__icon" />
+    return (
+        <div className="header">
+            <FlightIcon className="header__icon"/>
 
-      <div className="header__center">
-        <input type="text" placeholder="Search..." />
+            <div className="header__center">
+                <input type="text" placeholder="Search..."/>
 
-        <SearchIcon />
-      </div>
+                <SearchIcon/>
+            </div>
 
-      <div className="header__right">
-        <button id="loginbutton">Log In</button>
-        <button id="signupbutton">Sign Up</button>
-      </div>
-    </div>
-  );
+            <div className="header__right">
+                <Link to="/login">
+                    <button id="loginbutton">Log In</button>
+                </Link>
+                <Link to="/login">
+                <button id="signupbutton">Sign Up</button>
+                </Link>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
