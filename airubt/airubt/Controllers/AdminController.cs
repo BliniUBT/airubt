f66fb90e48f4544788b80e71abd5784f96ff4b27
DenisaDemaku <dd47597@ubt-uni.net>
2021-06-03 02:02:46 +0200

    Create AdminController.cs

---
+++ airubt/airubt/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using airubt.Application.Interfaces;
+using airubt.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace airubt.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HostController : ControllerBase
+    {
+        private IHostService _hostService;
+
+        public HostController(IHostService hostService)
+        {
+            _hostService = hostService;
+        }
+
+        [HttpGet("getHosts")]
+        public async Task<IActionResult> GetHosts() => Ok(await _hostService.HostsList());
+        [HttpPost("createHost")]
+        public async Task<IActionResult> CreateHost([FromBody] Host host)
+        {
+            _hostService.CreateHost(host);
+            return Ok();
+        }
+    }
+}
