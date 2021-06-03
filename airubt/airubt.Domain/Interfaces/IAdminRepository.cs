c67992b4580bba3a9047b8021c54117b0b075f33
DenisaDemaku <dd47597@ubt-uni.net>
2021-06-03 02:06:29 +0200

    Create IAdminRepository.cs

---
+++ airubt/airubt.Domain/Interfaces/IAdminRepository.cs
@@ -0,0 +1,18 @@
+﻿using airubt.Domain.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace airubt.Domain.Interfaces
+{
+    public interface IAdminRepository
+    {
+        Task<IEnumerable> GetAdmins();
+        Task<Admin> GetAdminById(int id);
+        void CreateAdmin(Admin admin);
+        Task UpdateAdmin(Admin admin);
+        Task<Admin> DeleteAdmin(int id);
+    }
+}
