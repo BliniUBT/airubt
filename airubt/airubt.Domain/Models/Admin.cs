c25c436f05698aed6288e0a1e4822275d8c91e72
DenisaDemaku <dd47597@ubt-uni.net>
2021-06-03 02:11:33 +0200

    Create Admin.cs

---
+++ airubt/airubt.Domain/Models/Admin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace airubt.Domain.Models
+{
+    public partial class Admin
+    {
+        public Admin()
+        {
+            Apartments = new HashSet<Apartment>();
+        }
+
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public virtual ICollection<Apartment> Apartments { get; set; }
+    }
+}
